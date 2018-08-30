aa2629c76b329ced37bbd74e0ce95ce26f7fe659
Tim Nolet <timnolet@hotmail.com>
2018-08-30 10:26:40 +0200

    feat: adds yearly pricing

--- src/js/index.js
+++ src/js/index.js
@@ -29,3 +29,51 @@ $(document).ready(() => {
       $('#browser-check-results').text(res.browserCheckResults)
     })
 })
+
+/**
+ * START Pricing
+ */
+
+const plans = [
+  {
+    name: 'starter',
+    year: 165,
+    month: 15
+  },
+  {
+    name: 'growth',
+    year: 440,
+    month: 40
+  },
+  {
+    name: 'business',
+    year: 880,
+    month: 80
+  }
+]
+
+$(document).ready(() => {
+  const monthly = $('#billing-monthly')
+  const annual = $('#billing-annual')
+
+  monthly.click(() => {
+    $('.toggle').toggleClass('toggle-selected')
+
+    for (let plan of plans) {
+      $(`[data-${plan.name}-price]`).text(plan.month)
+    }
+    $('[data-billing-interval]').text('/ month')
+
+  })
+  annual.click(() => {
+    $('.toggle').toggleClass('toggle-selected')
+
+    for (let plan of plans) {
+      $(`[data-${plan.name}-price]`).text(plan.year)
+    }
+    $('[data-billing-interval]').text('/ year')
+  })
+})
+/**
+ * END Pricing
+ */
